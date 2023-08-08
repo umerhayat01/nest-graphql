4cb872790be4aa8eba29faf4fc852b75e883b415
Umer Hayat <umer.hayat@kwanso.com>
2023-08-08 15:54:20 +0500

    add validation

--- src/pets/dto/create-pet.input.ts
+++ src/pets/dto/create-pet.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsAlpha } from 'class-validator';
 
 @InputType()
 export class CreatePetInput {
+  @IsAlpha()
   @Field()
   name: string;
 
